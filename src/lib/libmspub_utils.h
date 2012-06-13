bf1c8b0c4f24d932acce8d85e798c868ac5f3c7e
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-06-13 13:33:26 +0200

    Remove unused reader functions
    Easy to revert if they are actually needed

--- src/lib/libmspub_utils.h
+++ src/lib/libmspub_utils.h
@@ -41,8 +41,6 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned uint32_t;
-typedef int int32_t;
-typedef unsigned __int64 uint64_t;
 
 #else
 
@@ -93,8 +91,6 @@ uint32_t readU32(const unsigned char *input, unsigned offset);
 uint8_t readU8(WPXInputStream *input);
 uint16_t readU16(WPXInputStream *input);
 uint32_t readU32(WPXInputStream *input);
-uint64_t readU64(WPXInputStream *input);
-int32_t readS32(WPXInputStream *input);
 void readNBytes(WPXInputStream *input, unsigned long length, std::vector<unsigned char> &out);
 
 void appendCharacters(WPXString &text, std::vector<unsigned char> characters);

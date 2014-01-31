f65e2bc75d247044d8def59ced35cf1f63da71f3
Fridrich Štrba <fridrich.strba@bluewin.ch>
2014-01-31 19:31:03 +0100

    With agreement of all contributors, simplifying licensing to MPL2
    Change-Id: Ie051167443758c7db08072d5c322d38ac5ddc56a

--- src/lib/ColorReference.cpp
+++ src/lib/ColorReference.cpp
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libmspub project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "ColorReference.h"
 
 libmspub::Color libmspub::ColorReference::getRealColor(unsigned c, const std::vector<Color> &palette) const

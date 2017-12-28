LibreOffice/libmspub

commit 860883fa6fa861f808c091a3cecd03aa351d87a8

use existing conv. functions
Change-Id: Ia2f5ab36ae42321771fd43e77b96b5a0b9452412

src/lib/MSPUBCollector.cpp
@@ -310,11 +310,8 @@ void MSPUBCollector::setShapeStretchBorderArt(unsigned seqNum)
 
 void MSPUBCollector::setRectCoordProps(Coordinate coord, librevenge::RVNGPropertyList *props) const
 {
-  int xs = coord.m_xs, ys = coord.m_ys;
-  double x_center = m_width / 2;
-  double y_center = m_height / 2;
-  props->insert("svg:x", x_center + (double)xs / EMUS_IN_INCH);
-  props->insert("svg:y", y_center + (double)ys / EMUS_IN_INCH);
+  props->insert("svg:x", coord.getXIn(m_width));
+  props->insert("svg:y", coord.getYIn(m_width));
   props->insert("svg:width", coord.getWidthIn());
   props->insert("svg:height", coord.getHeightIn());
 }

commit 522860ad23e1aa368fe09885e7973fafc4d456e5

fix copypasta
Regression since commit 860883fa6fa861f808c091a3cecd03aa351d87a8 .

Change-Id: I36d26d8971891150afca060f86e73af953fab840

src/lib/MSPUBCollector.cpp
@@ -312,7 +312,7 @@ void MSPUBCollector::setShapeStretchBorderArt(unsigned seqNum)
 void MSPUBCollector::setRectCoordProps(Coordinate coord, librevenge::RVNGPropertyList *props) const
 {
   props->insert("svg:x", coord.getXIn(m_width));
-  props->insert("svg:y", coord.getYIn(m_width));
+  props->insert("svg:y", coord.getYIn(m_height));
   props->insert("svg:width", coord.getWidthIn());
   props->insert("svg:height", coord.getHeightIn());
 }